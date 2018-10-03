121aa478a68201897ec8e843e4100523413bb124
Vassu Manivasakan <vassu.manivasakan@gmail.com>
2018-10-03 16:59:37 +1000

    Input field requirements configured for creating memos - this will prevent invalid DB table entries

--- memo/Models/Memo.cs
+++ memo/Models/Memo.cs
@@ -2,14 +2,24 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
 
 namespace memo.Models
 {
     public class Memo
     {
-        public int MemoId { get; set; }
-        public string OwnerId { get; set; }
+        public int memoId { get; set; }
 
-        public string  Message { get; set; }
+        public string OwnerId { get; set; } //this field is used to tie to the login user
+        
+        [Required(ErrorMessage = "Please enter date and time")]
+        public DateTime Date { get; set; }
+
+        [Required(ErrorMessage = "Please enter title of memo")]
+        public string Title { get; set; }
+
+        [Required(ErrorMessage = "Please enter the description")]
+        public string Details { get; set; }
     }
+
 }
